45384bbf57cf43f5039571c83c65b51c9e21906a
hsnmtw <hussain.mutawa@gmail.com>
2024-03-04 02:38:13 +0300

    .

--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,3 @@
-alter table hr_document add [Status] varchar(30) not null default 'Pending';
+alter table [hr_Document]     add [Status]            varchar(30)        not null default 'Pending';
+alter table [hr_HRDepartment] add [PendingDocuments]  int                not null default 0;
+alter table [hr_Employee]     add [PendingDocuments]  int                not null default 0;
