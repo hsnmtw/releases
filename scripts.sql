5221e4d58f6bc5ffd5483fa6aefed5ea95f8062a
hsnmtw <hussain.mutawa@gmail.com>
2024-03-05 09:39:44 +0300

    ui

--- scripts.sql
+++
@@ -1,5 +0,0 @@
-insert into [common_ScheduledTask]
-([Name],[Frequency],[Time],[Enabled],[Procedure])
-values 
-('Daily System Upgrade','Day','04:00',1,'{ClassName:"ui.Helpers.UITaskExecution",Method:"RunUpgrade"}')
-;
