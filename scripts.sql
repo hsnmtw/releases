171e607039e1c5c2d673662cd129c56e79d922f6
hsnmtw <hussain.mutawa@gmail.com>
2024-03-06 13:07:49 +0300

    .

--- scripts.sql
+++ scripts.sql
@@ -1,4 +1,4 @@
-alter table [hr_Document] add [NumberOfDays] int not null default 0;
+--alter table [hr_Document] add [NumberOfDays] int not null default 0;
 --alter table [hr_Document] drop constraint [UK_hr_Document_EmployeeNumber_DocumentNumber];
 --alter table [hr_Document] add constraint [UK_hr_Document_EmployeeNumber_DocumentType_DocumentNumber] UNIQUE([EmployeeNumber],[DocumentType],[DocumentNumber]);
 --update [hr_Document] set [EmployeeName]=x.NameAr,[Department]=x.[Department] FROM [hr_Document] as y JOIN [hr_Employee] as x ON y.[EmployeeNumber]=x.[EmployeeNumber];
