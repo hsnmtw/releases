2fd18f06da87a1f3ea0d2a9c19fe2f6fda3940a2
hsnmtw <hussain.mutawa@gmail.com>
2024-03-09 10:47:04 +0300

    new release

--- scripts.sql
+++
@@ -1,4 +0,0 @@
-alter table [etc_user] add [ExpiryDate] DATETIME NOT NULL DEFAULT '2024-12-31';
-alter table [common_User] add [ExpiryDate] DATETIME NOT NULL DEFAULT '2024-12-31';
-update [etc_user] set [ExpiryDate]='2090-12-31' where username in ('habalmutawa','radawood');
-update [common_User] set [ExpiryDate]='2090-12-31' where username in ('habalmutawa','radawood');
