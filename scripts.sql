7a46993e0bd9b789f850400623f4ccc6e9fb05e7
hsnmtw <hussain.mutawa@gmail.com>
2024-03-11 17:59:18 +0300

    added fix for crashcart

---
+++ scripts.sql
@@ -0,0 +1,4 @@
+
+alter Table [crashcart_cart] alter column [SecurityCode]            VARCHAR(10)      NOT NULL;
+alter Table [crashcart_cart] alter column [CurrentSecurityCode]     VARCHAR(10)      ;
+alter Table [crashcart_cart] alter column [LastSecurityCode]        VARCHAR(10)      ;
