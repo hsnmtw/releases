712fea0a12c94ae869dd1a2388714fce892ea030
hsnmtw <hussain.mutawa@gmail.com>
2024-03-12 11:49:13 +0300

    .

--- scripts.sql
+++ scripts.sql
@@ -1,4 +1,5 @@
+sp_rename 'hr_employee.Cabenet','Cabinet','COLUMN';
 
-alter Table [crashcart_cart] alter column [SecurityCode]            VARCHAR(10)      NOT NULL;
-alter Table [crashcart_cart] alter column [CurrentSecurityCode]     VARCHAR(10)      ;
-alter Table [crashcart_cart] alter column [LastSecurityCode]        VARCHAR(10)      ;
+alter Table [crashcart_cart] alter column [SecurityCode]            VARCHAR(20)      NOT NULL;
+alter Table [crashcart_cart] alter column [CurrentSecurityCode]     VARCHAR(20)      ;
+alter Table [crashcart_cart] alter column [LastSecurityCode]        VARCHAR(20)      ;
