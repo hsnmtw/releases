7ad3cca02a4db9d288255103948a572a91954f3b
hsnmtw <hussain.mutawa@gmail.com>
2024-03-18 11:27:19 +0300

    .

--- scripts.sql
+++
@@ -1,3 +0,0 @@
-update etc_user set isenabled=0 where username in ('aalmouh','naldahama');
-update common_user set isenabled=0 where username in ('aalmouh','naldahama');
---insert into common_ScheduledTask ([Name],[Frequency],[Time],[Enabled],[Procedure]) VALUES('');
