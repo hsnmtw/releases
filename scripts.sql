c4a505632b29e6fbc1c39358fff873caa0b61dc1
hsnmtw <hussain.mutawa@gmail.com>
2024-03-20 15:47:15 +0300

    updated js scripts

--- scripts.sql
+++
@@ -1,7 +0,0 @@
-insert into [common_DomainGroup] ([Name],[FQDN],[IsAuthorized],[Owner]) VALUES ('HR Training','DB=HR Training',1,'salmordy');
-
-update etc_group set [FQDN]=[Name] where [FQDN] is null;
-update common_DomainGroup set [FQDN]=[Name] where [FQDN] is null;
-
-alter table etc_group alter column [FQDN] NVARCHAR(255) NOT NULL DEFAULT '!';
-alter table common_DomainGroup alter column [FQDN] NVARCHAR(255) NOT NULL DEFAULT '!';
